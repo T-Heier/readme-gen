146b2a73a33acc7b2000e5c5a99ae2f638479128
T-Heier <trentheier@gmail.com>
2021-04-17 19:22:08 -0400

    added functionality to write and create a new readme markdown file with the data from the prompt

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -57,7 +59,22 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+
+    .then((data) => {
+        const fileName = `${data.projectName}.md`
+        generateMarkdown(data)
+        const dataLicense = data.license
+        
+
+        fs.writeFile(fileName, (generateMarkdown(data)), (err) => {
+            if (err) {
+              console.error(err)
+            }
+        });
+    })
+}
 
 // Function call to initialize app
 init();
